0b339f5d84e166582dfba49e239b36f7818527f1
joaodearaujo <joao.pedrocdearaujo@gmail.com>
2026-06-10 18:42:40 -0300

    feat(constants): creates contants folder and add pages.ts

---
+++ front-end/src/constants/pages.ts
@@ -0,0 +1,6 @@
+import type { PageButtonProps } from "../types/page.type";
+
+export const pages: PageButtonProps[] = [
+    {name: 'daily', itemsDone: 0, totalItems: 24},
+    {name: 'weekly', itemsDone: 0, totalItems: 12},
+]
