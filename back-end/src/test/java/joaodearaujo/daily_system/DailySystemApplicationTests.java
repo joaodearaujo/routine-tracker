e2a41f7c119f0e3b4fc7aa826768fa36b721bbaf
joaodearaujo <joao.pedrocdearaujo@gmail.com>
2026-06-10 15:11:56 -0300

    feat(back-end): initialize back-end folder

---
+++ back-end/src/test/java/joaodearaujo/daily_system/DailySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package joaodearaujo.daily_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
