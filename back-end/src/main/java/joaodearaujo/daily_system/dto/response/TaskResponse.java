903f699b6788f52b33de5a4100da528bf0e56c2b
joaodearaujo <joao.pedrocdearaujo@gmail.com>
2026-06-14 14:39:25 -0300

    feat(dto): add TaskResponde DTO for task creation and update

--- back-end/src/main/java/joaodearaujo/daily_system/dto/response/TaskResponse.java
+++ back-end/src/main/java/joaodearaujo/daily_system/dto/response/TaskResponse.java
@@ -1,4 +1,10 @@
 package joaodearaujo.daily_system.dto.response;
+import joaodearaujo.daily_system.domain.enums.TaskTags;
 
-public class TaskResponse {
-}
+public record TaskResponse(
+        String id,
+        TaskTags tag,
+        String name,
+        String description,
+        Boolean isCore
+) {}
