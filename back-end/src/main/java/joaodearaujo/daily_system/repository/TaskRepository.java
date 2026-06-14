bb261279ec45147eb84ec26aee817f48bdfccf93
joaodearaujo <joao.pedrocdearaujo@gmail.com>
2026-06-14 14:40:49 -0300

    feat(repository): add TaskRepository for task persistence

--- back-end/src/main/java/joaodearaujo/daily_system/repository/TaskRepository.java
+++ back-end/src/main/java/joaodearaujo/daily_system/repository/TaskRepository.java
@@ -1,4 +1,7 @@
 package joaodearaujo.daily_system.repository;
 
-public interface TaskRepository {
+import joaodearaujo.daily_system.domain.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, String> {
 }
