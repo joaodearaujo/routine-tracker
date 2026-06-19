e3554afac8e37adace57e6b6180b27407a0193c2
joaodearaujo <joao.pedrocdearaujo@gmail.com>
2026-06-19 03:46:12 -0300

    chore: remove useIsComplete hook

--- front-end/src/features/routine/hooks/useIsComplete.ts
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export function useIsComplete() {
-     const [ isCompleted, setIsCompleted ] = useState(false);
-        setIsCompleted(prev => !prev);
-        return isCompleted;
-    };
