2ae31c928fc8c4d7c32bc4399eccf604a87f2bfc
joaodearaujo <joao.pedrocdearaujo@gmail.com>
2026-06-18 23:59:42 -0300

    refactor(front-end): remove old repository pattern and Daily page

--- front-end/src/features/routine/repositories/routine.repository.ts
+++
@@ -1,7 +0,0 @@
-import type { Routine, Task } from "../types/routine.domain";
-
-export interface RoutineRepository {
-  getAllRoutines(): Promise<Routine[]>;
-  getRoutineById(routineId: string): Promise<Routine>;
-  getTasksByGroupId(groupId: string): Promise<Task[]>;
-}
